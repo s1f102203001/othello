81337f3421142984688620a16b09511fc0c07b21
s1f102203001 <s1f102203001@iniad.org>
2024-06-08 04:31:02 +0900

    駒置けるように

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,201 +1,42 @@
+import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
+  const [turnColor, setTurnColor] = useState(1);
+  const [board, setBoard] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const clikHandler = (x: number, y: number) => {
+    console.log(x, y);
+    const newBoard = structuredClone(board);
+    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
+      newBoard[y][x] = turnColor;
+    }
+    setTurnColor(2 / turnColor);
+    setBoard(newBoard);
+  };
   return (
     <div className={styles.container}>
       <div className={styles.boardStyle}>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
-        <div className={styles.cellStyle}>
-          <div className={styles.cellStone} />
-        </div>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div className={styles.cellStyle} key={`${x}-${y}`} onClick={() => clikHandler(x, y)}>
+              {color !== 0 && (
+                <div
+                  className={styles.cellStone}
+                  style={{ background: color === 1 ? '#000' : '#fff' }}
+                />
+              )}
+            </div>
+          )),
+        )}
       </div>
     </div>
   );

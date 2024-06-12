84ba9ecbd6b3efd802a65bcfa5fe26eb9f6b0935
s1f102203001 <s1f102203001@iniad.org>
2024-06-13 05:39:05 +0900

    点数表示まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,17 +13,79 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+
+  const directions = [
+    [0, 1],
+    [-1, 1],
+    [1, 0],
+    [1, -1],
+    [1, 1],
+    [-1, -1],
+    [-1, 0],
+    [0, -1],
+  ];
+
+  const countStones = (board: number[][]) => {
+    let blackCount = 0;
+    let whiteCount = 0;
+
+    for (const row of board) {
+      for (const cell of row) {
+        if (cell === 1) blackCount++;
+        if (cell === 2) whiteCount++;
+      }
+    }
+
+    return { blackCount, whiteCount };
+  };
+
+  const { blackCount, whiteCount } = countStones(board);
+
   const clikHandler = (x: number, y: number) => {
     console.log(x, y);
     const newBoard = structuredClone(board);
-    if (board[y + 1] !== undefined && board[y + 1][x] === 2 / turnColor) {
-      newBoard[y][x] = turnColor;
+    if (newBoard[y][x] !== 0) {
+      return;
     }
-    setTurnColor(2 / turnColor);
-    setBoard(newBoard);
+    let validMove = false;
+    for (const dir of directions) {
+      let x_a = x + dir[0];
+      let y_a = y + dir[1];
+      const tilesToFlip = [];
+      while (
+        x_a >= 0 &&
+        x_a < 8 &&
+        y_a >= 0 &&
+        y_a < 8 &&
+        newBoard[y_a][x_a] !== 0 &&
+        newBoard[y_a][x_a] !== turnColor
+      ) {
+        if (newBoard[y_a][x_a] !== turnColor) {
+          tilesToFlip.push([x_a, y_a]);
+          x_a += dir[0];
+          y_a += dir[1];
+          if (x_a >= 0 && x_a < 8 && y_a >= 0 && y_a < 8 && newBoard[y_a][x_a] === turnColor) {
+            validMove = true;
+            for (const [flipX, flipY] of tilesToFlip) {
+              newBoard[flipY][flipX] = turnColor;
+            }
+          }
+        }
+      }
+    }
+    if (validMove) {
+      newBoard[y][x] = turnColor;
+      setTurnColor(turnColor === 1 ? 2 : 1);
+      setBoard(newBoard);
+    }
   };
   return (
     <div className={styles.container}>
+      <div className={styles.scoreBoard}>
+        <span>
+          黒: {blackCount} 白: {whiteCount}
+        </span>
+      </div>
       <div className={styles.boardStyle}>
         {board.map((row, y) =>
           row.map((color, x) => (
